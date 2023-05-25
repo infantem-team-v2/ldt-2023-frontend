95a5fec38fbc108c93f952834a4ca2a1db5df609
Kirill Shiryaev <infantem.help@gmail.com>
2023-05-25 06:09:58 +0300

    ff

--- src/components/BasicCalculator.js
+++ src/components/BasicCalculator.js
@@ -243,7 +243,7 @@ const BasicCalculator = () => {
         label={element.comment}
         formLabel={element.field}
         value={fields[fieldId]}
-        onChange={() => { updateFieldsStates(fieldId, !fields[fieldId]) }}
+        onChange={(e) => { updateFieldsStates(fieldId, !e.target.checked) }}
         overlay={renderTooltip(element.comment)}
       />
     )
