272f9b2ecbcd3489bb7c6bdc3964ce844e71ebdc
Kirill Shiryaev <infantem.help@gmail.com>
2023-05-26 22:21:13 +0300

    updates

--- src/components/Pages/F404Page.js
+++ src/components/Pages/F404Page.js
@@ -7,11 +7,12 @@ import "../../styles/F404Page.css";
 
 const F404Page = () => {
   return (
-    <div className="container text-center mt-4">
+    <div className="text-center page-404-container">
       <h1 className="h1-404">Ooops...</h1>
       <h1 className="h1-404">Что-то пошло не так</h1>
       <F404Svg className="img-fluid img-404" />
-      <p className="p-404">Произошла ошибка 404 <br />Страница не найдена</p>
+      <p className="p-404 ">Произошла ошибка 404</p>
+      <p className="p-404 ">Страница не найдена</p>
     </div>
   );
 };
--- src/components/Pages/MainPage.js
+++ src/components/Pages/MainPage.js
@@ -10,18 +10,21 @@ import RegularMultipleDropdown from "../ui-kit/RegularMultipleDropdown";
 
 const MainPage = () => {
 
+  const currentScreen = window.screen.width;
+
 
   return (
     <>
       <div className="m-5 mt-4 main-div">
-        <div className="d-flex justify-content-between ">
+        <div className={currentScreen > 768 ? "d-flex justify-content-between " : ""}>
           <div className="p-logo ">Калькулятор для предпринимателей </div>
-          <div className="p-logo secondary-head-sign">Быстрый и удобный инструмент для московских предпринимателей, расчитайте инвестиции в два клика!</div>
+          {currentScreen > 768 ? < div className="p-logo secondary-head-sign">Быстрый и удобный инструмент для московских предпринимателей, расчитайте инвестиции в два клика!</div> : <></>}
         </div>
         <h1 className="h1-main">Расчитай вложения <HeaderThing /></h1>
+        {currentScreen <= 768 ? < div className="p-logo secondary-head-sign">Быстрый и удобный инструмент для московских предпринимателей, расчитайте инвестиции в два клика!</div> : <></>}
         < BasicCalculator />
         <DistrictsMap />
-      </div>
+      </div >
     </>
   )
 };
