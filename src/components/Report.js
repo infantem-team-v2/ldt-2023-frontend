5392a0fafb82b003d7c951ff1cc0a1933ae44687
Kirill Shiryaev <infantem.help@gmail.com>
2023-05-28 02:43:57 +0300

    ff

--- src/components/Report.js
+++ src/components/Report.js
@@ -48,6 +48,12 @@ const Report = ({ isLogedIn }) => {
     }
   }
 
+  const convertText = (text) => {
+    const words = text.split('_');
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(' ');
+  }
+
 
 
 
