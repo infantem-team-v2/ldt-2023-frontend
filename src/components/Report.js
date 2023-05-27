02f5d0f9ec6e8d90ff3ad5d7e4ad5667d006b0b3
Kirill Shiryaev <infantem.help@gmail.com>
2023-05-28 02:47:23 +0300

    ff

--- src/components/Report.js
+++ src/components/Report.js
@@ -49,9 +49,14 @@ const Report = ({ isLogedIn }) => {
   }
 
   const convertText = (text) => {
-    const words = text.split('_');
-    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-    return capitalizedWords.join(' ');
+    if (text.includes('_')) {
+      const words = text.split('_');
+      const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+      return capitalizedWords.join(' ');
+    } else {
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+
   }
 
 
