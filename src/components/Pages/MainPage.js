337bfdeb75c6dac6c094cbace392c9f47de95f19
Kirill Shiryaev <infantem.help@gmail.com>
2023-05-28 06:01:59 +0300

    ff

--- src/components/Pages/MainPage.js
+++ src/components/Pages/MainPage.js
@@ -4,13 +4,13 @@ import BasicCalculator from "../BasicCalculator";
 import { ReactComponent as HeaderThing } from '../../asserts/header_thing.svg';
 
 import '../../styles/MainPage.css';
-import RegularSlider from "../ui-kit/RegularSlider";
+
 
 const MainPage = ({ isLogedIn }) => {
 
   const currentScreen = window.screen.width;
 
-  const [fields, setFields] = React.useState();
+
 
 
   return (
@@ -25,14 +25,7 @@ const MainPage = ({ isLogedIn }) => {
         < BasicCalculator isLogedIn={isLogedIn} />
 
       </div >
-      <RegularSlider
-        controlId='accounting-operations'
-        label='Количество бухгалтерских операций'
-        value={fields}
-        onChange={(e) => { setFields(e.target.value) }}
-        overlay={<p>Количество бухгалтерских операций в месяц'</p>}
-        formLabel={"Количество бухгалтерских операций: "}
-      />
+
     </>
   )
 };
